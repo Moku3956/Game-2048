9771e82db3756dd0635b3a3492817b89f33c15d4
Moku3956 <moku3956@icloud.com>
2025-12-13 09:45:35 +0900

    タイルのロジック追加

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { Grid } from "./Grid"
+import { Grid } from "./Grid";
 import { InputManager } from "./InputManager";
 
 const grid = new Grid();
 grid.addRandomTile();
-grid.render()
+grid.render();
 const inputManager = new InputManager();
-inputManager.setup()
+inputManager.setup();
 
 inputManager.onCall = (direction: string) => {
-  console.log(direction);
-  grid.moveTile(direction);  
-  // renderメソッドとaddRandomTileメソッドの実行する順番を変えるな
-  grid.addRandomTile();  
-  grid.render();
+  if (grid.moveTile(direction)) {
+    // renderメソッドとaddRandomTileメソッドの実行する順番を変えるな
+    grid.addRandomTile();
+    grid.render();
+  }
 };
